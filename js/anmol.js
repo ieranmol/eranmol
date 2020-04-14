d2c1c31e43f9016106e8fa02326c3ffb6d08837c
ieranmol <51746249+ieranmol@users.noreply.github.com>
2020-04-14 23:35:16 +0530

    Add files via upload

---
+++ js/anmol.js
@@ -0,0 +1,11 @@
+var aContact= new Contact("Ludhiana, India","contact@eranmol.com","+91 98880 33499"," Senior Mechanical Engineer "," A.L.Coldforge Pvt. Ltd.","Mechanical Engineer")
+function Contact(aAddress, aEmail, aNumber, aPosition, aCompany, aTitle)
+	{
+		this.address = aAddress;
+		this.email = aEmail;
+		this.number = aNumber;
+		this.position = aPosition;
+		this.company = aCompany;
+		this.title = aTitle;
+
+}
