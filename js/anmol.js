1fd712bfcfadd6ca4c4ba6b15dd404db42a5d9de
ieranmol <51746249+ieranmol@users.noreply.github.com>
2021-07-03 22:07:53 -0400

    Add files via upload

--- js/anmol.js
+++ js/anmol.js
@@ -1,4 +1,4 @@
-var aContact= new Contact("Brampton, ON, Canada","contact@eranmol.com","‭(***)***-**99‬"," Setup Mechanic "," Jones Healthcare Group.","Mechanical Engineer")
+var aContact= new Contact("Brampton, ON, Canada","contact@eranmol.com","‭(519) 657-8479‬"," Setup Mechanic "," Jones Healthcare Group.","Mechanical Engineer")
 function Contact(aAddress, aEmail, aNumber, aPosition, aCompany, aTitle)
 	{
 		this.address = aAddress;
